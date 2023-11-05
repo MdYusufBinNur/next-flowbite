b7bcde7d57fe8c265e38375fe2098169ddf2d617
admin <mdyusufbinnur@gmail.com>
2023-11-05 15:50:21 +0600

    initial

---
+++ app/components/header.tsx
@@ -0,0 +1,9 @@
+import { DarkThemeToggle, Navbar } from "flowbite-react";
+import Image from "next/image";
+import { FC } from "react";
+import { useSidebarContext } from "../context/SidebarContext";
+
+const Header: FC<Record<string, never>> = function () {
+};
+
+export default Header;
--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,244 @@
-import Image from 'next/image'
+"use client";
 
-export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+import {
+    Accordion,
+    Alert,
+    Avatar,
+    Sidebar as FlowbiteSidebar,
+} from "flowbite-react";
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+import React, {useState} from "react";
+import {BiBuoy} from "react-icons/bi";
+import {
+    HiArrowSmRight,
+    HiChartPie,
+    HiEye,
+    HiInbox,
+    HiShoppingBag,
+    HiTable,
+    HiUser,
+    HiViewBoards,
+} from "react-icons/hi";
+import Header from "./components/header";
+import Sidebar from "./components/sidebar";
+import {SidebarProvider} from "./context/SidebarContext";
+import {AlertFunction} from "@/app/components/Alert";
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+export default function Index(): JSX.Element {
+    return (
+        <SidebarProvider>
+            <Header/>
+            <div className="flex">
+                <div className="order-1">
+                    <ActualSidebar/>
+                </div>
+                <main className="order-2 mx-4 mt-4 mb-24 flex-[1_0_16rem]">
+                    <HomePage/>
+                </main>
+            </div>
+        </SidebarProvider>
+    );
 }
+
+function ActualSidebar(): JSX.Element {
+    return (
+        <Sidebar>
+            <Sidebar.Items>
+                <Sidebar.ItemGroup>
+                    <Sidebar.Item href="#" icon={HiChartPie}>
+                        Dashboard
+                    </Sidebar.Item>
+                    <Sidebar.Item href="#" icon={HiViewBoards}>
+                        Kanban
+                    </Sidebar.Item>
+                    <Sidebar.Item href="#" icon={HiInbox}>
+                        Inbox
+                    </Sidebar.Item>
+                    <Sidebar.Item href="#" icon={HiUser}>
+                        Users
+                    </Sidebar.Item>
+                    <Sidebar.Item href="#" icon={HiShoppingBag}>
+                        Products
+                    </Sidebar.Item>
+                    <Sidebar.Item href="#" icon={HiArrowSmRight}>
+                        Sign In
+                    </Sidebar.Item>
+                    <Sidebar.Item href="#" icon={HiTable}>
+                        Sign Up
+                    </Sidebar.Item>
+                </Sidebar.ItemGroup>
+                <Sidebar.ItemGroup>
+                    <Sidebar.Item href="#" icon={HiChartPie}>
+                        Upgrade to Pro
+                    </Sidebar.Item>
+                    <Sidebar.Item href="#" icon={HiViewBoards}>
+                        Documentation
+                    </Sidebar.Item>
+                    <Sidebar.Item href="#" icon={BiBuoy}>
+                        Help
+                    </Sidebar.Item>
+                </Sidebar.ItemGroup>
+            </Sidebar.Items>
+        </Sidebar>
+    );
+}
+
+function HomePage() {
+    return (
+        <div className="p-6">
+            <section>
+                <header>
+                    <h1 className="mb-6 text-5xl font-extrabold dark:text-white">
+                        Welcome to <code>Flowbite</code> on <code>Next.js</code>!
+                    </h1>
+                </header>
+            </section>
+            <section>
+                <header>
+                    <h2 className="mb-3 text-4xl font-bold dark:text-gray-200">Alert</h2>
+                </header>
+                <AlertFunction/>
+            </section>
+        </div>
+    );
+}
+
+function AccordionExample(): JSX.Element {
+    return (
+        <Accordion flush>
+            <Accordion.Panel>
+                <Accordion.Title>What is Flowbite?</Accordion.Title>
+                <Accordion.Content>
+                    <p className="mb-2 text-gray-500 dark:text-gray-400">
+                        Flowbite is an open-source library of interactive components built
+                        on top of Tailwind CSS including buttons, dropdowns, modals,
+                        navbars, and more.
+                    </p>
+                    <p className="text-gray-500 dark:text-gray-400">
+                        Check out this guide to learn how to{" "}
+                        <a
+                            href="https://flowbite.com/docs/getting-started/introduction/"
+                            className="text-blue-600 hover:underline dark:text-blue-500"
+                        >
+                            get started
+                        </a>{" "}
+                        and start developing websites even faster with components on top of
+                        Tailwind CSS.
+                    </p>
+                </Accordion.Content>
+            </Accordion.Panel>
+            <Accordion.Panel>
+                <Accordion.Title>Is there a Figma file available?</Accordion.Title>
+                <Accordion.Content>
+                    <p className="mb-2 text-gray-500 dark:text-gray-400">
+                        Flowbite is first conceptualized and designed using the Figma
+                        software so everything you see in the library has a design
+                        equivalent in our Figma file.
+                    </p>
+                    <p className="text-gray-500 dark:text-gray-400">
+                        Check out the{" "}
+                        <a
+                            href="https://flowbite.com/figma/"
+                            className="text-blue-600 hover:underline dark:text-blue-500"
+                        >
+                            Figma design system
+                        </a>{" "}
+                        based on the utility classes from Tailwind CSS and components from
+                        Flowbite.
+                    </p>
+                </Accordion.Content>
+            </Accordion.Panel>
+            <Accordion.Panel>
+                <Accordion.Title>
+                    What are the differences between Flowbite and Tailwind UI?
+                </Accordion.Title>
+                <Accordion.Content>
+                    <p className="mb-2 text-gray-500 dark:text-gray-400">
+                        The main difference is that the core components from Flowbite are
+                        open source under the MIT license, whereas Tailwind UI is a paid
+                        product. Another difference is that Flowbite relies on smaller and
+                        standalone components, whereas Tailwind UI offers sections of pages.
+                    </p>
+                    <p className="mb-2 text-gray-500 dark:text-gray-400">
+                        However, we actually recommend using both Flowbite, Flowbite Pro,
+                        and even Tailwind UI as there is no technical reason stopping you
+                        from using the best of two worlds.
+                    </p>
+                    <p className="mb-2 text-gray-500 dark:text-gray-400">
+                        Learn more about these technologies:
+                    </p>
+                    <ul className="list-disc pl-5 text-gray-500 dark:text-gray-400">
+                        <li>
+                            <a
+                                href="https://flowbite.com/pro/"
+                                className="text-blue-600 hover:underline dark:text-blue-500"
+                            >
+                                Flowbite Pro
+                            </a>
+                        </li>
+                        <li>
+                            <a
+                                href="https://tailwindui.com/"
+                                rel="nofollow"
+                                className="text-blue-600 hover:underline dark:text-blue-500"
+                            >
+                                Tailwind UI
+                            </a>
+                        </li>
+                    </ul>
+                </Accordion.Content>
+            </Accordion.Panel>
+        </Accordion>
+    );
+}
+
+function AlertsExample(): JSX.Element {
+    return (
+        <Alert
+            color="success"
+            rounded={false}
+            withBorderAccent
+            onDismiss={console.log}
+            additionalContent={
+                <React.Fragment>
+                    <div className="mt-2 mb-4 text-sm text-green-700 dark:text-green-800">
+                        More info about this info alert goes here. This example text is
+                        going to run a bit longer so that you can see how spacing within an
+                        alert works with this kind of content.
+                    </div>
+                    <div className="flex">
+                        <button
+                            type="button"
+                            className="mr-2 inline-flex items-center rounded-lg bg-green-700 px-3 py-1.5 text-center text-xs font-medium text-white hover:bg-green-800 focus:ring-4 focus:ring-green-300 dark:bg-green-800 dark:hover:bg-green-900"
+                        >
+                            <HiEye className="-ml-0.5 mr-2 h-4 w-4"/>
+                            View more
+                        </button>
+                        <button
+                            type="button"
+                            className="rounded-lg border border-green-700 bg-transparent px-3 py-1.5 text-center text-xs font-medium text-green-700 hover:bg-green-800 hover:text-white focus:ring-4 focus:ring-green-300 dark:border-green-800 dark:text-green-800 dark:hover:text-white"
+                        >
+                            Dismiss
+                        </button>
+                    </div>
+                </React.Fragment>
+            }
+        >
+            <h3 className="text-lg font-medium text-green-700 dark:text-green-800">
+                This is a info alert
+            </h3>
+        </Alert>
+    );
+}
+
+function AvatarExample(): JSX.Element {
+    return (
+        <Avatar
+            bordered
+            img="https://flowbite.com/docs/images/people/profile-picture-5.jpg"
+            rounded
+        />
+    );
+}
+
