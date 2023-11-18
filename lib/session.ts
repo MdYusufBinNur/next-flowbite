5830f27fe905d40341c5c5c7597bebc5ba0914d8
admin <mdyusufbinnur@gmail.com>
2023-11-18 19:08:46 +0600

    initial

--- lib/session.ts
+++ lib/session.ts
@@ -3,6 +3,7 @@ import { getServerSession } from "next-auth/next";
 
 export async function getCurrentUser() {
   const session = await getServerSession(options);
-  console.log('session : ', session)
+  // console.log('session : ', session)
+
   return session?.user;
 }
